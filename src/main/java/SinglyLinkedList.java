e60b4362cb2ba4af3da2381169bae7e2c1c423ab
jordan_ren <jordanr2025@student.cis.edu.hk>
2024-01-25 09:10:38 +0800

    finished

--- src/main/java/SinglyLinkedList.java
+++ src/main/java/SinglyLinkedList.java
@@ -1,3 +1,4 @@
+
 public class SinglyLinkedList<T> {
 
     // Inner Node class.
@@ -11,8 +12,27 @@ public class SinglyLinkedList<T> {
         // Constructor of the Node class.
         // The constructor should set the data property of the Node to be the value passed as a parameter.
         // The constructor should set the next property of the Node to be null.
+        private T data;
+        private Node<T> next;
 
+        public Node(T data){
+            this.data = data;
+            this.next = null;
+        }
+        public T getData() {
+            return data;
+        }
 
+        public Node<T> getNext() {
+            return next;
+        }
+        public void setData(T data) {
+            this.data = data;
+        }
+
+        public void setNext(Node<T> next) {
+            this.next = next;
+        }
     }
 
     // Properties of the Singly Linked List class.
@@ -24,7 +44,15 @@ public class SinglyLinkedList<T> {
 
     // Constructor.
     // Creates a Singly Linked List with a head node.
+
+    private int size;
+    private Node<T> head;
+    private Node<T> tail;
+
     public SinglyLinkedList(T value) {
+        head = new Node<>(value);
+        tail = head;
+        size = 1;
 
     }
 
@@ -33,48 +61,56 @@ public class SinglyLinkedList<T> {
     // size
     // returns the size of the Singly Linked List.
     public int size() {
-
-        return 0;
-
+        return size;
     }
 
     // isEmpty
     // returns whether the Singly Linked List is empty.
     public boolean isEmpty() {
-
-        return false;
-
+        return size == 0;
     }
 
     // peekFirst
     // returns the data stored in the head node.
     // throws a run time exception if the Singly Linked List is empty.
     public T peekFirst() {
-
-        return null;
-
+        return head.getData();
     }
 
     // peekLast
     // returns the data stored in the tail node.
     // throws a run time exception if the Singly Linked List is empty.
     public T peekLast() {
-
-        return null;
-
+        return tail.getData();
     }
 
     // addFirst
     // Adds a node to the front of the Singly Linked List.
     // If the Singly Linked List is empty,
     public void addFirst(T value) {
-
+        Node<T> newNode = new Node<>(value);
+        if (isEmpty()) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.setNext(head);
+            head = newNode;
+        }
+        size++;
     }
 
     // addLast
     // Adds a node to the back of the Singly Linked List.
     public void addLast(T value) {
-
+        Node<T> newNode = new Node<>(value);
+        if (isEmpty()) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.setNext(newNode);
+            tail = newNode;
+        }
+        size++;
     }
 
     // insertAt
@@ -83,7 +119,25 @@ public class SinglyLinkedList<T> {
     // If the index is equal to size, then we can invoke the addLast method.
     // throws an illegal argument exception if the index is invalid.
     public void insert(T value, int index) {
-
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException();
+        }
+        if (index == 0) {
+            addFirst(value);
+            return;
+        }
+        if (index == size) {
+            addLast(value);
+            return;
+        }
+        Node<T> current = head;
+        for (int i = 1; i < index; i++) {
+            current = current.getNext();
+        }
+        Node<T> newNode = new Node<>(value);
+        newNode.setNext(current.getNext());
+        current.setNext(newNode);
+        size++;
     }
 
     // removeFirst
@@ -92,9 +146,15 @@ public class SinglyLinkedList<T> {
     // Returns the data stored in the head node.
     // If the size of the Singly Linked List becomes 0, need to set the tail to null.
     public T removeFirst() {
-
-        return null;
-
+        T data = head.getData();
+        if (size == 1) {
+            head = null;
+            tail = null;
+        } else {
+            head = head.getNext();
+        }
+        size--;
+        return data;
     }
 
     // removeLast
@@ -103,8 +163,22 @@ public class SinglyLinkedList<T> {
     // Returns the data stored in the tail node.
     // If the size of the Singly Linked List becomes 0, need to set the tail to null.
     public T removeLast() {
-
-        return null;
+        if (size == 1) {
+            T data = head.getData();
+            head = null;
+            tail = null;
+            size--;
+            return data;
+        }
+        Node<T> current = head;
+        for (int i = 1; i < size - 1; i++) {
+            current = current.getNext();
+        }
+        T data = current.getNext().getData();
+        tail = current;
+        tail.setNext(null);
+        size--;
+        return data;
 
     }
 
@@ -116,8 +190,23 @@ public class SinglyLinkedList<T> {
     // throws an illegal argument exception if the index is invalid.
 
     public T removeAt(int index) {
-
-        return null;
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException();
+        }
+        if (index == 0) {
+            return removeFirst();
+        }
+        if (index == size - 1) {
+            return removeLast();
+        }
+        Node<T> current = head;
+        for (int i = 1; i < index; i++) {
+            current = current.getNext();
+        }
+        T data = current.getNext().getData();
+        current.setNext(current.getNext().getNext());
+        size--;
+        return data;
 
     }
 
@@ -125,7 +214,13 @@ public class SinglyLinkedList<T> {
     // Determines whether the Singly Linked List contains a node that holds data equivalent to the value passed.
     // Returns a boolean.
     public boolean contains(T value) {
-
+        Node<T> current = head;
+        while (current != null) {
+            if (current.getData().equals(value)) {
+                return true;
+            }
+            current = current.getNext();
+        }
         return false;
 
     }
@@ -134,23 +229,53 @@ public class SinglyLinkedList<T> {
     // Returns the data held in the node at a specified index.
     // Throws an illegal argument exception if the index is invalid.
     public T valueAt(int index) {
-
-        return null;
-
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException();
+        }
+        Node<T> current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.getNext();
+        }
+        return current.getData();
     }
 
     // reverse
     // Reverses the Singly Linked List.
     public void reverse() {
-
+        if (isEmpty() || size == 1) {
+            return;
+        }
+        Node<T> current = head;
+        Node<T> previous = null;
+        Node<T> next;
+        while (current != null) {
+            next = current.getNext();
+            current.setNext(previous);
+            previous = current;
+            current = next;
+        }
+        head = previous;
     }
 
     // toString
     // Returns a String representation of the Singly Linked List.
     public String toString() {
-
-        return null;
-
+        StringBuilder sb = new StringBuilder();
+        Node<T> current = head;
+        if(size == 1){
+            sb.append(current.getData());
+            sb.append(" -> null");
+            return sb.toString();
+        }
+        while (current != null) {
+                sb.append(current.getData());
+                if (current.getNext() != null) {
+                    sb.append(" -> ");
+                }
+                current = current.getNext();
+        }
+        sb.append(" -> null");
+        return sb.toString();
     }
 
 }
